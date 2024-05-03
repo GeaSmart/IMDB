1b992b650b6a7a129fa334e9bc50d00fb59e53ed
Gerson Azabache <gerson_dunk@hotmail.com>
2024-05-03 08:06:36 -0500

    first controller, starting out

---
+++ IMDB.Api/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using IMDB.Application.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMDB.Api.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            this.movieRepository = movieRepository;
+        }
+    }
+}
