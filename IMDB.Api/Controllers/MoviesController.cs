c747e6bee9dd3a8f888108a5dad00ce15f014df0
Gerson Azabache <gazabache@unifiedsoftware.us>
2024-05-06 14:59:05 -0500

    mappings strategy done

--- IMDB.Api/Controllers/MoviesController.cs
+++ IMDB.Api/Controllers/MoviesController.cs
@@ -1,9 +1,13 @@
-﻿using IMDB.Application.Repositories;
+﻿using IMDB.Api.Mapping;
+using IMDB.Application.Models;
+using IMDB.Application.Repositories;
+using IMDB.Contracts.Requests;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IMDB.Api.Controllers
 {
     [ApiController]
+    [Route("api")]
     public class MoviesController : ControllerBase
     {
         private readonly IMovieRepository movieRepository;
@@ -11,6 +15,14 @@ namespace IMDB.Api.Controllers
         public MoviesController(IMovieRepository movieRepository)
         {
             this.movieRepository = movieRepository;
+        }
+
+        [HttpPost("movies")]
+        public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+        {
+            var movie = request.MapToMovie();
+            await movieRepository.CreateAsync(movie);
+            return Created($"/api/movies/{movie.Id}", movie);
         }
     }
 }
---
+++ IMDB.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,19 @@
+﻿using IMDB.Application.Models;
+using IMDB.Contracts.Requests;
+
+namespace IMDB.Api.Mapping
+{
+    public static class ContractMapping
+    {
+        public static Movie MapToMovie(this CreateMovieRequest request)
+        {
+            return new Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList()
+            };
+        }
+    }
+}
